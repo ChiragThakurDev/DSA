52af3fd6fdcd90649d79e51b5a5e691abdbdd76b
Chirag <chirag212003@gmail.com>
2026-05-06 22:09:53 +0530

    Done

---
+++ RECURSION/Phase_2/Q4.cpp
@@ -0,0 +1,15 @@
+//Find product of digits of a number recursively.
+#include<iostream>
+using namespace std;
+int prod(int n){
+    if(n<10) return n;
+
+    return (n%10)*prod(n/10);
+}
+int main(){
+
+   cout<<prod(345)<<endl;
+   cout<<prod(2)<<endl;
+    cout<<prod(2346)<<endl;
+    return 0;
+}
