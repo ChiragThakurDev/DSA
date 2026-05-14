781f8955ac7dcbbb800817a6152533029ad78feb
Chirag <chirag212003@gmail.com>
2026-05-14 22:04:25 +0530

    Done

---
+++ RECURSION/Phase_4/Q2.cpp
@@ -0,0 +1,20 @@
+//Check if a string is palindrome using recursion.
+#include <iostream>
+using namespace std;
+bool check(string &s, int start, int end)
+{
+    if (start >= end)
+        return true;
+    if (s[start] != s[end])
+        return false;
+    return check(s, start + 1, end - 1);
+}
+int main()
+{
+    string s = "racecar";
+    if (check(s, 0, s.length() - 1))
+        cout << "Yes it is a palindrome";
+    else
+        cout << "No it is not a palindrome";
+    return 0;
+}
