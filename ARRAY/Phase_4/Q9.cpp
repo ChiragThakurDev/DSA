a9ed05198af6ac658313d7f116948ac610a83075
ChiragThakurDev <chirag212003@gmail.com>
2026-06-06 08:01:36 -0400

    Count the frequency array of a numbers

---
+++ ARRAY/Phase_4/Q9.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+
+int main() {
+    int n;
+
+    std::cout << "Enter the size of array: ";
+    std::cin >> n;
+
+    std::vector<int> arr(n);
+
+    std::cout << "Enter the elements of the array: ";
+    for(int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+    }
+
+    int maxVal = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+    }
+
+    std::vector<int> freq(maxVal + 1, 0);
+
+    // Count frequencies
+    for(int i = 0; i < n; i++) {
+        freq[arr[i]]++;
+    }
+
+    // Print frequencies
+    for(int i = 0; i <= maxVal; i++) {
+        if(freq[i] > 0) {
+            std::cout << "Frequency of " << i
+                      << " = " << freq[i] << std::endl;
+        }
+    }
+
+    return 0;
+}
