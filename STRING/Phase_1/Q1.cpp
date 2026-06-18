64cce61f5c14b3c33609ff6974284dbee3ab26d3
ChiragThakurDev <chirag212003@gmail.com>
2026-06-18 01:07:27 -0400

    Take a string and print its length

---
+++ STRING/Phase_1/Q1.cpp
@@ -0,0 +1,8 @@
+//Take a string input and print its length
+#include<iostream>
+#include <string>
+int main(){
+    std::string str="Hi how are you and what are you doing";
+    std::cout<<str.length()<<" ";
+    return 0;
+}
