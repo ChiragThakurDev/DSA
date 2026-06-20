4a5f624635f8fb3a3d7265c57cf3cb8155ba19c5
ChiragThakurDev <chirag212003@gmail.com>
2026-06-20 10:10:11 -0400

    Take two strings and print them in concatenated

---
+++ STRING/Phase_1/Q7.cpp
@@ -0,0 +1,11 @@
+//Take two string and print them concatenated
+#include<iostream>
+#include<string>
+
+int main(){
+    std::string str1="Hello Buddy! ";
+    std::string str2="How are you.";
+    std::string result=str1 + str2;
+    std::cout<<result<<std::endl;
+    return 0;
+}
